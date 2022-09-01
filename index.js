ab2a62e8945c75e90643795ec691507a86c134ad
momospnr <h.tagawa@momoirospanner.app>
2022-09-01 11:24:08 +0900

    feat: optimize for scraping the site

--- index.js
+++ index.js
@@ -1,9 +1,40 @@
-const { chromium, firefox, webkit } = require('playwright');
+const { chromium } = require('playwright');
 
 (async () => {
-  const browser = await chromium.launch();  // Or 'firefox' or 'webkit'.
+  const browser = await chromium.launch();
   const page = await browser.newPage();
-  await page.goto('http://example.com');
-  // other actions...
+  await page.goto(""); // URLを渡す
+  const title = await page.title();
+  await page.waitForLoadState();
+  await page.click("#onetrust-reject-all-handler");
+  await page.addStyleTag({
+    path: "pdf.css"
+  });
+  // 1ページ目
+  await page.pdf({
+    path: `dist/【1ページ目】${title}.pdf`,
+    printBackground: true,
+    pageRanges: "1-1"
+  });
+  // 2ページ目以降
+  await page.pdf({
+    path: `dist/【2ページ目以降】${title}.pdf`,
+    printBackground: true,
+    displayHeaderFooter: true,
+    headerTemplate: `
+    <style>
+    html {
+      -webkit-print-color-adjust: exact;
+    }
+    </style>
+    <div style="width: 100%; background-color: #ffffff; margin-top: -20px; height: 170px;">
+      <p style="width: 100%; background-color: #666666; height: 81px;"></p>
+    </div>
+   `,
+    margin: {
+      top: 170
+    },
+    pageRanges: "2-"
+  });
   await browser.close();
 })();
